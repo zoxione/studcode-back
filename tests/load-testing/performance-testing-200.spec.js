05026e4e24e4ef2dd8a5c46507690c3ecf380153
Bair <73276298+zoxione@users.noreply.github.com>
2024-06-03 19:57:42 +0300

    added load testing

---
+++ tests/load-testing/performance-testing-200.spec.js
@@ -0,0 +1,36 @@
+// k6 run performance-testing-200.spec.js
+
+import http from 'k6/http';
+import { sleep } from 'k6';
+import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.2/index.js';
+
+export const options = {
+  insecureSkipTLSVerify: true,
+  noConnectionReuse: false,
+  stages: [
+    { duration: '30s', target: 200 },
+    { duration: '2m', target: 200 },
+    { duration: '30s', target: 0 },
+  ],
+  thresholds: {
+    http_req_duration: ['p(95)<250'],
+  },
+};
+
+const API_URL = 'http://localhost:5050';
+
+export default function () {
+  http.batch([
+    ['GET', `${API_URL}/api/v1/projects`],
+    ['GET', `${API_URL}/api/v1/tags`],
+    ['GET', `${API_URL}/api/v1/users`],
+  ]);
+  sleep(1);
+}
+
+export function handleSummary(data) {
+  return {
+    stdout: textSummary(data, { indent: ' ', enableColors: true }),
+    'performance-testing-200.json': JSON.stringify(data['metrics'], null, 2),
+  };
+}
---
+++ tests/load-testing/stability-testing.spec.js
@@ -0,0 +1,33 @@
+// k6 run stability-testing.spec.js
+
+import http from 'k6/http';
+import { sleep } from 'k6';
+import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.2/index.js';
+
+export const options = {
+  insecureSkipTLSVerify: true,
+  noConnectionReuse: false,
+  stages: [
+    { duration: '2m', target: 120 },
+    { duration: '3h56m', target: 120 },
+    { duration: '2m', target: 0 },
+  ],
+};
+
+const API_URL = 'http://localhost:5050';
+
+export default function () {
+  http.batch([
+    ['GET', `${API_URL}/api/v1/projects`],
+    ['GET', `${API_URL}/api/v1/tags`],
+    ['GET', `${API_URL}/api/v1/users`],
+  ]);
+  sleep(1);
+}
+
+export function handleSummary(data) {
+  return {
+    stdout: textSummary(data, { indent: ' ', enableColors: true }),
+    'stability-testing.json': JSON.stringify(data['metrics'], null, 2),
+  };
+}
